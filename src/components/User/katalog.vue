d84ba04ecbd8b7970a6a8b37259bf0fb1ab83900
Calvindo <50109685+ryune12@users.noreply.github.com>
2020-12-02 00:17:05 +0700

    merge ging

--- src/components/User/katalog.vue
+++ src/components/User/katalog.vue
@@ -1,181 +1,155 @@
 <template>
-  <div> 
-
-    <v-app-bar app absolute height="75px" style="background: rgba(61, 30, 26, 0.8);">
-      
-
-    <v-avatar
-      color="primary"
-      size="40"
-      tile
-    ></v-avatar>
-      <h3 style="color:white;">   Perpustakaan Tadika Mesra</h3>
+  <div>
+    <v-app-bar
+      app
+      absolute
+      height="75px"
+      style="background: rgba(61, 30, 26, 0.8)"
+    >
+      <v-avatar color="primary" size="40" tile></v-avatar>
+      <h3 style="color: white">Perpustakaan Tadika Mesra</h3>
       <V-Spacer />
-    <v-btn-toggle
+      <v-btn-toggle
         v-model="navbarBtn"
         tile
         color="white accent-3"
         rounded
         group
-
       >
         <v-btn value="home" @click="pindahPage(1)">
-          <span class="whiteText">
-            Home
-          </span>
+          <span class="whiteText"> Home </span>
         </v-btn>
-
 
         <v-btn value="katalog" @click="pindahPage(2)">
-          <span class="whiteText">
-            Katalog
-          </span>
-        </v-btn>
-    
-        <v-btn value="profil" @click="pindahPage(3)">
-          <span class="whiteText">
-            Profil
-          </span>
+          <span class="whiteText"> Katalog </span>
         </v-btn>
 
+        <v-btn value="profil" @click="pindahPage(3)">
+          <span class="whiteText"> Profil </span>
+        </v-btn>
       </v-btn-toggle>
       <V-Spacer />
 
       <v-btn rounded @click="pindahPage(4)">Login/Register</v-btn>
-
     </v-app-bar>
 
-  <v-card>
-      <v-img 
+    <v-card>
+      <v-img
         src="https://wallpapercave.com/wp/IlpLRYK.jpg"
-        gradient="to top right, rgba(0, 0, 0, 0.70), rgba(0, 0, 0, 0.70)">
+        gradient="to top right, rgba(0, 0, 0, 0.70), rgba(0, 0, 0, 0.70)"
+      >
         <v-card
           class="mx-auto"
           max-width="auto"
-         flat
-          style="margin-top: 10%;background: rgba(0, 0, 0, 0);"
-          
+          flat
+          style="margin-top: 10%; background: rgba(0, 0, 0, 0)"
         >
-            <v-toolbar
-              dense
-              width="50%"
-              style="margin-start: 26%;"
-              
-            >
-              <v-text-field
-                hide-details
-                label="Cari Buku"
-                single-line
-                
-              ></v-text-field>
+          <v-toolbar dense width="50%" style="margin-start: 26%">
+            <v-text-field
+              hide-details
+              label="Cari Buku"
+              single-line
+            ></v-text-field>
 
-              <v-btn icon>
-                <v-icon>mdi-magnify</v-icon>
-              </v-btn>
+            <v-btn icon>
+              <v-icon>mdi-magnify</v-icon>
+            </v-btn>
 
-              <v-select
-                :items="items"
-                hide-details
-                label="Filled style"
-                style="width:0.5%;"
-                single-line
-                ></v-select>
-
-            </v-toolbar>
-
-            
+            <v-select
+              :items="items"
+              hide-details
+              label="Filled style"
+              style="width: 0.5%"
+              single-line
+            ></v-select>
+          </v-toolbar>
         </v-card>
-      <v-card v-for="data in books"
-      :key="data"
-      class="mx-auto"
-      max-width="auto"
-      flat
-     style="margin-top: 10px;background: rgba(0, 0, 0, 0);"
-      >
-       <v-card
-          width="50%"
-          style="margin-start: 26%;"              
+        <v-card
+          v-for="data in books"
+          :key="data.index"
+          class="mx-auto"
+          max-width="auto"
+          flat
+          style="margin-top: 10px; background: rgba(0, 0, 0, 0)"
         >
-              <v-layout row>
-                <v-flex xs7>
-                  <v-card-title primary-title>
-                    <div>
-                      <div class="headline">{{data.Judul}}</div>
-                      <div>{{data.pengarang}}</div>
-                      <div>{{data.tahun}}</div>
-                    </div>
-                  </v-card-title>
-                </v-flex>
-                <v-flex xs5>
-                  <v-img
-                    v-bind:src="'http://127.0.0.1:8000/' +data.image"
-                    style="width:100px;height:100px;"
-                    
-                  ></v-img>
-                </v-flex>
-              </v-layout>
-              <v-divider light></v-divider>
-              <v-card-actions class="pa-3">
-                Penerbit: {{data.penerbit}}
-                <v-spacer></v-spacer>
-                <v-btn color="brown" @click="pinjamBuku(data.id)">Pinjam</v-btn>
-              </v-card-actions>
-       </v-card>
-      </v-card>
+          <v-card width="50%" style="margin-start: 26%">
+            <v-layout row>
+              <v-flex xs7>
+                <v-card-title primary-title>
+                  <div>
+                    <div class="headline">{{ data.Judul }}</div>
+                    <div>{{ data.pengarang }}</div>
+                    <div>{{ data.tahun }}</div>
+                  </div>
+                </v-card-title>
+              </v-flex>
+              <v-flex xs5>
+                <v-img
+                  v-bind:src="'http://127.0.0.1:8000/' + data.image"
+                  style="width: 100px; height: 100px"
+                ></v-img>
+              </v-flex>
+            </v-layout>
+            <v-divider light></v-divider>
+            <v-card-actions class="pa-3">
+              Penerbit: {{ data.penerbit }}
+              <v-spacer></v-spacer>
+              <v-btn color="brown" @click="pinjamBuku(data)">Pinjam</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-card>
       </v-img>
-</v-card>
+    </v-card>
 
-  <v-snackbar v-model="snackbar" :color="color" timeout="2000" bottom>
-            {{ error_message }}
-          </v-snackbar>
-</div>
-
-
+    <v-snackbar v-model="snackbar" :color="color" timeout="20000" bottom>
+      {{ error_message }}
+    </v-snackbar>
+  </div>
 </template>
 
 <style>
-
-.whiteText{
-   color: whitesmoke;
+.whiteText {
+  color: whitesmoke;
 }
-
-
 </style>
 
 
 <script>
-  export default {
-    data: () => ({
-      sticky: false,
-      navbarBtn: 'katalog',
-      items: ['Anak', 'Fantasi', 'Pendidikan', 'Novel'],
-      books:[],
-      pinjam:[],
-      snackbar:false,
-    }),
+export default {
+  data: () => ({
+    sticky: false,
+    navbarBtn: "katalog",
+    items: ["Anak", "Fantasi", "Pendidikan", "Novel"],
+    books: [],
+    pinjam: [],
+    tgl_pinjam: "",
+    peminjam: "",
+    snackbar: false,
+    error_message: "",
+    color: "",
+  }),
 
-    methods:{
-      pindahPage(nomor){
-        if(nomor==1){
-            this.$router.push({
-              name: 'index'
-            })
-        }else if(nomor==2)
-        {
-              this.$router.push({
-              name: 'katalog'
-            })
-        }else if(nomor==3){
-              this.$router.push({
-              name: 'profil'
-            })
-        }else{
-              this.$router.push({
-              name: 'login'
-            })
-        }
-      },
-      readData() {
+  methods: {
+    pindahPage(nomor) {
+      if (nomor == 1) {
+        this.$router.push({
+          name: "index",
+        });
+      } else if (nomor == 2) {
+        this.$router.push({
+          name: "katalog",
+        });
+      } else if (nomor == 3) {
+        this.$router.push({
+          name: "profil",
+        });
+      } else {
+        this.$router.push({
+          name: "login",
+        });
+      }
+    },
+    readData() {
       var url = this.$api + "/buku";
       this.$http
         .get(url, {
@@ -185,6 +159,7 @@
         })
         .then((response) => {
           this.books = response.data.data;
+          console.log(this.books);
         })
         .catch((error) => {
           this.error_message = error.response.data.message;
@@ -192,45 +167,35 @@
           this.snackbar = true;
         });
     },
-    pinjamBuku(id){
-
-      this.$http.get(this.$api + '/buku/'+ id,{
-         headers: {
+    post(url, data) {
+      this.$http
+        .post(url, data, {
+          headers: {
             Authorization: "Bearer " + localStorage.getItem("token"),
           },
-      })
-      .then((response) => {
-          this.pinjam = response.data.data;
+        })
+        .then((response) => {
+          this.error_message = response.data.message;
+          this.color = "green";
+          this.snackbar = true;
+          this.load = false;
+          this.readData();
         })
         .catch((error) => {
           this.error_message = error.response.data.message;
           this.color = "red";
           this.snackbar = true;
-        });
-
-        this.$http.post(this.$api+'/request',{
-          'Judul': this.pinjam.Judul,
-          'ISBN': this.pinjam.ISBN,
-          'peminjam': localStorage.getItem("id"),
-          'tgl_pinjam': new Date().toLocaleString(),
-
-        },{
-           headers: {
-            Authorization: "Bearer " + localStorage.getItem("token"),
-          },
-        }).then((response) => {
-          this.pinjam = response.data.data;
-        })
-        .catch((error) => {
-          this.error_message = error.response.data.message;
-          this.color = "red";
-          this.snackbar = true;
+          this.load = false;
         });
     },
-    
+
+    pinjamBuku(item) {
+      var url = this.$api + "/request";
+      this.post(url, item);
     },
-    mounted(){
-      this.readData()
-    }
-  }
+  },
+  mounted() {
+    this.readData();
+  },
+};
 </script>
